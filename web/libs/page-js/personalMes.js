32d6830778cadc0888f48bb87758627367846a58
chensuyun <867937611@qq.com>
2017-07-31 08:54:06 +0800

    管理地址修改

--- web/libs/page-js/personalMes.js
+++ web/libs/page-js/personalMes.js
@@ -10,7 +10,18 @@ require(['config'],()=>{
 		$('.head_portrait').children().last().on('touchend',function(){
 			$('.head_portrait').fadeOut();
 		})
-		
+		/*$('.head_portrait').children().eq(1).on('touchend',function(){
+			console.log(66);
+			$(this).diyUpload({
+				url:'server/fileupload.php',
+				success:function( data ) {
+					console.info( data );
+				},
+				error:function( err ) {
+					console.info( err );	
+				}
+			});
+		})*/
 		$('.extra').children().eq(4).on('touchend',function(){
 			location.href = 'site.html';
 		})
--- web/libs/page-js/site.js
+++ web/libs/page-js/site.js
@@ -1,16 +1,38 @@
 require(['config'],()=>{
 	require(['jquery'],($)=>{
+		//先获取localStorage
+		var user = localStorage.user;
+		if(user){
+			user = JSON.parse(user)
+			console.log(user);
+		}else{
+			user = [];
+		}
+		var res = user.map(function(i){
+			return`
+				<div class="administration">
+					<div class="one">
+						<span class="name">${i.username}</span>
+						<span class="phone">${i.phone}</span>
+					</div>
+					<div class="site">${i.sheng}${i.shi}${i.qu}${i.site}</div>
+					<div class="compile">
+						<span class="delect">删除</span>
+						<div class="redact">
+							<span>编辑</span>
+							<span>确定</span>
+						</div>
+					</div>
+				</div>
+			`
+		})
+		$('.body').html(res);
 		//跳到主页
 		$('.button').on('touchend',function(){
 			location.href = "index.html";
 		})
-		$('.total_price').on('touchend',function(){
-			$('.head_portrait').fadeIn();
-		})
-		$('.head_portrait').children().last().on('touchend',function(){
-			$('.head_portrait').fadeOut();
-		})
-		$('.compile').children().eq(1).on('touchend',function(){
+		//编辑按钮编辑
+		$('.redact').children().eq(0).on('touchend',function(){
 			//获取site里面的值内容
 			var message = $('.site').html();
 			$('.site').html("<input type='text' class='xiugai'/>");
@@ -18,19 +40,19 @@ require(['config'],()=>{
 			$('.redact').children().eq(0).css({display:'none'});
 			$('.redact').children().eq(1).css({display:'block'});
 		})
+		//确定按钮
 		$('.redact').children().eq(1).on('touchend',function(){
 			var val = $('.site input').val();
-			console.log(val);
-			console.log($('.xiugai'));
-			$('.xiugai').remove();
 			$('.site').html(val);
-			console.log($('.redact').children().eq(0))
+			$('.xiugai').remove();
 			$('.redact').children().eq(0).css({display:'block'});
+			$('.redact').children().eq(1).css({display:'none'});
 		})
+		//删除按钮删除信息
 		$('.compile').on('touchend','.delect',function(){
-			var index = $(this).parent('.administration').index();
-			console.log(index);
-			$('.administration').eq(index).remove();
+			console.log($(this).parent().parent());
+			$(this).parent().parent().remove();
 		})
+		
 	})
 })
